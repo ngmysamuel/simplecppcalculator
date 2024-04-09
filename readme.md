ea157b64b912c0254755400996e777c3ad58bde5
ngmysamuel <e0148675@u.nus.edu>
2024-04-09 22:59:37 +0800

    Update readme and refactored main.cpp

--- readme.md
+++ readme.md
@@ -1,9 +1,46 @@
-# Building and Running locally
-* `git clone https://github.com/ngmysamuel/simplecppcalculator.git`
-*  `g++ src/app.cpp -o out -lws2_32`
-* `./out.exe`
+# Simple CPP Calculator
+A simple API you can call to add, subtract, multiply, and divide. Plus, exponents.  
 
-# Building and Running using GitHub Actions
+Health Check
+```
+GET https://docker-repo-i4yya62lxq-as.a.run.app/
+```  
+
+Add
+```
+GET https://docker-repo-i4yya62lxq-as.a.run.app/plus/<arg1>/<arg2>
+```
+
+Subtract
+```
+GET https://docker-repo-i4yya62lxq-as.a.run.app/minus/<arg1>/<arg2>
+```
+
+Multiply
+```
+GET https://docker-repo-i4yya62lxq-as.a.run.app/multiply/<arg1>/<arg2>
+```
+
+Divide
+```
+GET https://docker-repo-i4yya62lxq-as.a.run.app/divide/<arg1>/<arg2>
+```
+
+Exponent
+```
+GET https://docker-repo-i4yya62lxq-as.a.run.app/exponent/<arg1>/<arg2>
+```
+
+# Building and Running
+
+## Locally
+```
+git clone https://github.com/ngmysamuel/simplecppcalculator.git
+g++ src/app.cpp -o out -lws2_32
+./out.exe
+```
+
+## GitHub Actions
 * Simply push!
     * Step 1: Checkout.
         * This step checks out the code - similar to what you would do in the local build and run step
@@ -21,16 +58,16 @@
 
 ![Workflow Diagram](photo_3.png)
 
-## Troubles faced
+## Issues faced
 * Google's documentation on permissioning for GitHub Actions was extremely hard to understand
     * [Workload Identity Federation](https://cloud.google.com/iam/docs/workload-identity-federation)
     * Creation of service accounts, Workload identity pools, Workload identity pool providers, OIDC, local JWKs, attribute mappings, CEL expressions, etc
-* After deploying to Cloud Run, there was an extra step needed to disable Authentication. 
-* Small issues 
+* Assorted issues 
     * outdated tutorials
     * wrong issuer URI when setting up Federated Identity
     * wrong URI concatenation (missing image name) in docker build/push
     * understanding what Google's Artifact Repository's location meant
+    * after deploying to Cloud Run, there was an extra step needed to disable Authentication
 * Had to grant permissions but was looking in the wrong place
 ![Where I was stuck](photo_1.png)
 Actual page to grant the roles was at another page  
@@ -39,6 +76,10 @@ Actual page to grant the roles was at another page
 ## Helpful Links
 * [Where to get C++ compiler](https://www.mingw-w64.org/downloads/)
 * [How to download w64devkit](https://github.com/skeeto/w64devkit)
+* [Containerising a C++ application](https://medium.com/codex/a-practical-guide-to-containerize-your-c-application-with-docker-50abb197f6d4)
+* [Understanding roles of CMake, make and GCC](https://stackoverflow.com/q/39761924)
+* [cpp-httplib](https://github.com/yhirose/cpp-httplib)
+*[Header files](https://learn.microsoft.com/en-us/cpp/cpp/header-files-cpp?view=msvc-170)
 
 ### Less Helpful Links
 * [Setting up Identity Federation and allowing GitHub Actions](https://cloud.google.com/blog/products/identity-security/secure-your-use-of-third-party-tools-with-identity-federation)
@@ -46,3 +87,19 @@ Actual page to grant the roles was at another page
 * [Deployment Specific - did not really make use of this](https://cloud.google.com/blog/products/devops-sre/deploy-to-cloud-run-with-github-actions)
 * [GitHub's perspective on the first link - correct URI found here](https://docs.github.com/en/actions/deployment/security-hardening-your-deployments/configuring-openid-connect-in-google-cloud-platform)
 * [List of GCP Locations](https://cloud.google.com/artifact-registry/docs/repositories/repo-locations)
+
+### Some commands used
+```
+gcloud iam workload-identity-pools providers create-oidc github-actions-oidc \
+--workload-identity-pool="github-actions-pool" \
+--issuer-uri="https://token.actions.githubusercontent.com" \
+--attribute-mapping="google.subject=assertion.sub,attribute.repository=assertion.repository,attribute.repository_owner=assertion.repository_owner,attribute.branch=assertion.sub.extract('/heads/{branch}/')" \
+--location=global \
+--attribute-condition="attribute.owner==assertion.repository_owner"
+
+gcloud iam service-accounts create deployer-sa --display-name="Deployer Service Account" --description="Deploys the application for you"
+
+gcloud iam service-accounts add-iam-policy-binding deployer-sa@orbital-lacing-281401.iam.gserviceaccount.com \
+  --role="roles/iam.workloadIdentityUser" \
+--member="principal://iam.googleapis.com/projects/473066013520/locations/global/workloadIdentityPools/github-actions-pool/subject/repo:ngmysamuel/devops_apr_24:ref:refs/heads/main"
+```
--- src/app.cpp
+++ src/app.cpp
@@ -2,8 +2,37 @@
 #include "./httplib.h"
 #include <math.h>
 
+std::string formatJson(std::string ans, std::string op, std::string arg1, std::string arg2) {
+    return "{\"answer\": \"" + ans + "\", \"statement\": \"" + arg1 + " " + op + " " + arg2 + " = " + ans + "\" }";
+}
+
+std::string operate(std::string arg1, std::string arg2, std::string op) {
+    double first_argument = std::stod( arg1 );
+    double second_argument = std::stod( arg2 );
+    if (op == "plus") {
+        auto answer = std::to_string( first_argument + second_argument );
+        return formatJson(answer, "+", arg1, arg2);
+    } else if (op == "minus") {
+        auto answer = std::to_string( first_argument - second_argument );
+        return formatJson(answer, "-", arg1, arg2);
+    } else if (op == "multiply") {
+        auto answer = std::to_string( first_argument * second_argument );
+        return formatJson(answer, "*", arg1, arg2);
+    } else if (op == "divide") {
+        auto answer = std::to_string( first_argument / second_argument );
+        return formatJson(answer, "/", arg1, arg2);
+    }
+    throw std::invalid_argument( "No such operator" );
+}
+
+void handleException(std::exception& ex, httplib::Response &res) {
+    std::cout << "\nInvalid... Exception: " << ex.what() << std::endl;
+    res.set_content("{\"isError\": \"true\", \"message\": \"Invalid... Please check server logs.\"}", "application/json");
+    res.status = httplib::StatusCode::InternalServerError_500;
+}
+
 int main() {
-    std::cout << "Hi there!";
+    std::cout << "Server started!";
 
     // HTTP
     httplib::Server svr;
@@ -14,49 +43,45 @@ int main() {
 
     svr.Get("/plus/:first/:second", [](const httplib::Request& req, httplib::Response &res) {
         try {
-            double first_argument = std::stod( req.path_params.at("first") );
-            double second_argument = std::stod( req.path_params.at("second") );
-            auto answer = std::to_string( first_argument + second_argument );
-            res.set_content(req.path_params.at("first") + " + " + req.path_params.at("second")  + " = " + answer, "text/plain");
+            res.set_content(
+                    operate(req.path_params.at("first"), req.path_params.at("second"), "plus"),
+                    "application/json"
+                );
         } catch (std::exception& ex) {
-            std::cout << ex.what() << std::endl;
-            res.set_content("Invalid... Please check server logs.", "text/plain");
+            handleException(ex, res);
         }
     });
 
     svr.Get("/divide/:first/:second", [](const httplib::Request& req, httplib::Response &res) {
         try {
-            double first_argument = std::stod( req.path_params.at("first") );
-            double second_argument = std::stod( req.path_params.at("second") );
-            auto answer = std::to_string( first_argument / second_argument );
-            res.set_content(req.path_params.at("first") + " / " + req.path_params.at("second")  + " = " + answer, "text/plain");
+            res.set_content(
+                    operate(req.path_params.at("first"), req.path_params.at("second"), "divide"),
+                    "application/json"
+                );
         } catch (std::exception& ex) {
-            std::cout << ex.what() << std::endl;
-            res.set_content("Invalid... Please check server logs.", "text/plain");
+            handleException(ex, res);
         }
     });
 
     svr.Get("/minus/:first/:second", [](const httplib::Request& req, httplib::Response &res) {
         try {
-            double first_argument = std::stod( req.path_params.at("first") );
-            double second_argument = std::stod( req.path_params.at("second") );
-            auto answer = std::to_string( first_argument - second_argument );
-            res.set_content(req.path_params.at("first") + " - " + req.path_params.at("second")  + " = " + answer, "text/plain");
+            res.set_content(
+                    operate(req.path_params.at("first"), req.path_params.at("second"), "minus"),
+                    "application/json"
+                );
         } catch (std::exception& ex) {
-            std::cout << ex.what() << std::endl;
-            res.set_content("Invalid... Please check server logs.", "text/plain");
+            handleException(ex, res);
         }
     });
 
     svr.Get("/multiply/:first/:second", [](const httplib::Request& req, httplib::Response &res) {
         try {
-            double first_argument = std::stod( req.path_params.at("first") );
-            double second_argument = std::stod( req.path_params.at("second") );
-            auto answer = std::to_string( first_argument * second_argument );
-            res.set_content(req.path_params.at("first") + " x " + req.path_params.at("second")  + " = " + answer, "text/plain");
+            res.set_content(
+                    operate(req.path_params.at("first"), req.path_params.at("second"), "multiply"),
+                    "application/json"
+                );
         } catch (std::exception& ex) {
-            std::cout << ex.what() << std::endl;
-            res.set_content("Invalid... Please check server logs.", "text/plain");
+            handleException(ex, res);
         }
     });
 
@@ -65,17 +90,18 @@ int main() {
             double first_argument = std::stod( req.path_params.at("first") );
             double second_argument = std::stod( req.path_params.at("second") );
             if (first_argument > 50) {
-                res.set_content("Please select a number less than 50", "text/plain");
+                res.set_content("{\"isError\": \"true\", \"message\": \"Invalid... Please select a number less than 50\"}", "application/json");
+                res.status = httplib::StatusCode::InternalServerError_500;
             }
             else if (second_argument > 50) {
-                res.set_content("Please select a number less than 50", "text/plain");
+                res.set_content("{\"isError\": \"true\", \"message\": \"Invalid... Please select a number less than 50\"}", "application/json");
+                res.status = httplib::StatusCode::InternalServerError_500;
             } else {
                 auto answer = std::to_string( pow(first_argument, second_argument) );
-                res.set_content(req.path_params.at("first") + " ** " + req.path_params.at("second")  + " = " + answer, "text/plain");
+                res.set_content(formatJson(answer, "**", req.path_params.at("first"), req.path_params.at("second")), "application/json");
             }
         } catch (std::exception& ex) {
-            std::cout << ex.what() << std::endl;
-            res.set_content("Invalid... Please check server logs.", "text/plain");
+            handleException(ex, res);
         }
     });
 
